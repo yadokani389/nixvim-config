098149e8d89331fdc0cad51ff18481c87384f2e0
yadokani389 <yadokani389@gmail.com>
2024-04-14 18:09:48 +0900

    change clangd settings

--- config/lsp.nix
+++ config/lsp.nix
@@ -1,9 +1,21 @@
-{
+{ pkgs, options, ... }: {
   plugins.lsp = {
     enable = true;
     servers = {
       bashls.enable = true;
-      clangd.enable = true;
+      clangd = {
+        enable = true;
+        cmd = [
+          "${options.plugins.lsp.servers.clangd.package.default}/bin/clangd"
+          "--header-insertion=iwyu"
+          "--background-index"
+          "--clang-tidy"
+          "--all-scopes-completion"
+          "--completion-style=detailed"
+          "--function-arg-placeholders"
+          "--fallback-style=google"
+        ];
+      };
       elixirls.enable = true;
       fsautocomplete.enable = true;
       gopls.enable = true;
