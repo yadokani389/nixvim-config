cd7c743144fe6dd8fe02ed51d8050701e1092852
yadokani389 <yadokani389@gmail.com>
2024-05-03 22:55:12 +0900

    Add clangd-extensions

--- config/lsp.nix
+++ config/lsp.nix
@@ -1,27 +1,33 @@
 { pkgs, options, ... }: {
-  plugins.lsp = {
-    enable = true;
-    servers = {
-      bashls.enable = true;
-      clangd.enable = true;
-      elixirls.enable = true;
-      fsautocomplete.enable = true;
-      gopls.enable = true;
-      kotlin-language-server.enable = true;
-      nixd.enable = true;
-      ruff-lsp.enable = true;
-    };
-    keymaps = {
-      lspBuf = {
-        "<leader>ld" = "definition";
-        "<leader>lR" = "references";
-        "<leader>lt" = "type_definition";
-        "<leader>li" = "implementation";
-        "<leader>lh" = "hover";
-        "<leader>lr" = "rename";
-        "<leader>lf" = "format";
+  plugins = {
+    lsp = {
+      enable = true;
+      servers = {
+        clangd = {
+          enable = true;
+          onAttach.function = ''
+            require("clangd_extensions.inlay_hints").setup_autocmd()
+            require("clangd_extensions.inlay_hints").set_inlay_hints()
+          '';
+        };
+        gopls.enable = true;
+        kotlin-language-server.enable = true;
+        nixd.enable = true;
+        ruff-lsp.enable = true;
+      };
+      keymaps = {
+        lspBuf = {
+          "<leader>ld" = "definition";
+          "<leader>lR" = "references";
+          "<leader>lt" = "type_definition";
+          "<leader>li" = "implementation";
+          "<leader>lh" = "hover";
+          "<leader>lr" = "rename";
+          "<leader>lf" = "format";
+        };
       };
     };
+    clangd-extensions.enable = true;
+    rust-tools.enable = true;
   };
-  plugins.rust-tools.enable = true;
 }
