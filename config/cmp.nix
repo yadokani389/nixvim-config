c81bef1526e3a1be2805b0899674b61c11b303c0
yadokani389 <yadokani389@gmail.com>
2024-08-19 22:38:56 +0900

    cmp: change keymaps

--- config/cmp.nix
+++ config/cmp.nix
@@ -108,12 +108,10 @@
         preselect = "cmp.PreselectMode.None";
 
         mapping = {
-          "<C-n>" = "cmp.mapping.select_next_item()";
-          "<C-p>" = "cmp.mapping.select_prev_item()";
           "<C-j>" = "cmp.mapping.select_next_item()";
           "<C-k>" = "cmp.mapping.select_prev_item()";
-          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<C-n>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-p>" = "cmp.mapping.scroll_docs(4)";
           "<C-Space>" = "cmp.mapping.complete()";
           "<C-e>" = "cmp.mapping.close()";
           "<CR>" =
