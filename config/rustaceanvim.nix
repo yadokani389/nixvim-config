87a66acff052b7560d3a362eda08bdc7d8ada922
yadokani389 <yadokani389@gmail.com>
2024-11-29 22:28:08 +0900

    rustaceanvim: use lldb instead of vscode-lldb

--- config/rustaceanvim.nix
+++ config/rustaceanvim.nix
@@ -1,22 +1,11 @@
 { pkgs, ... }: {
   plugins.rustaceanvim = {
     enable = true;
-    settings = {
-      dap = {
-        autoloadConfigurations = true;
-        adapter = let code-lldb = pkgs.vscode-extensions.vadimcn.vscode-lldb;
-        in {
-          executable.command =
-            "${code-lldb}/share/vscode/extensions/vadimcn.vscode-lldb/adapter/codelldb";
-          executable.args = [
-            "--liblldb"
-            "${code-lldb}/share/vscode/extensions/vadimcn.vscode-lldb/lldb/lib/liblldb.dylib"
-            "--port"
-            "13000"
-          ];
-          type = "server";
-          port = "13000";
-        };
+    settings.dap = {
+      autoloadConfigurations = true;
+      adapter = {
+        command = "${pkgs.lldb_19}/bin/lldb-dap";
+        type = "executable";
       };
     };
   };
