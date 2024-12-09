f7029413d844b5abfd56cea0bf9758d7719349ba
yadokani389 <yadokani389@gmail.com>
2024-12-09 22:56:52 +0900

    rust-analyzer: update for cmp
    https://github.com/rust-lang/rust-analyzer/issues/18536

--- flake.nix
+++ flake.nix
@@ -14,7 +14,32 @@
   outputs = { nixpkgs, nixvim, flake-utils, neovim-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (final: prev: {
+              rust-analyzer-unwrapped =
+                prev.rust-analyzer-unwrapped.overrideAttrs (drv: rec {
+                  version = "2024-12-09";
+                  name = "${drv.pname}-${version}";
+
+                  src = pkgs.fetchFromGitHub {
+                    owner = "rust-lang";
+                    repo = "rust-analyzer";
+                    rev = version;
+                    hash =
+                      "sha256-I1uc97f/cNhOpCemIbBAUS+CV0R7jts0NW9lc8jrpxc=";
+                  };
+                  cargoDeps = drv.cargoDeps.overrideAttrs {
+                    inherit src;
+                    name = "${name}-vendor.tar.gz";
+                    outputHash =
+                      "sha256-8hqUopbagXlZcIf8wcibMT1zHZrVuStm3lMGAWlMTY4=";
+                  };
+                });
+            })
+          ];
+        };
         config = import ./config { inherit pkgs neovim-overlay; };
         nixvim' = nixvim.legacyPackages.${system};
         nvim = nixvim'.makeNixvimWithModule {
