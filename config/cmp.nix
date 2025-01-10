57b9499e263f9f1a3e68f068036fe0503166c4ef
yadokani389 <yadokani389@gmail.com>
2025-01-10 19:52:24 +0900

    chore: update

--- config/cmp.nix
+++ config/cmp.nix
@@ -147,8 +147,10 @@
     copilot-cmp.enable = true;
     copilot-lua = {
       enable = true;
-      suggestion = { enabled = false; };
-      panel = { enabled = false; };
+      settings = {
+        suggestion.enabled = false;
+        panel.enabled = false;
+      };
     };
     copilot-chat.enable = true;
   };
