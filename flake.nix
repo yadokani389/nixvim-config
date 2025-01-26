611ebfad5b47238769c080f990c303f1ef8c6a18
yadokani389 <yadokani389@gmail.com>
2025-01-26 21:24:12 +0900

    deno: use latest

--- flake.nix
+++ flake.nix
@@ -10,28 +10,7 @@
   outputs = { nixpkgs, nixvim, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            (final: prev: {
-              deno = prev.deno.overrideAttrs (old: rec {
-                version = "2.1.5";
-                src = prev.fetchFromGitHub {
-                  owner = "denoland";
-                  repo = "deno";
-                  tag = "v${version}";
-                  hash = "sha256-CeRMcMuwER6LnLGAheGS+v4FDw7KADefB3p5ve1HsfE=";
-                };
-                cargoDeps = old.cargoDeps.overrideAttrs (_: {
-                  inherit src;
-                  outputHash =
-                    "sha256-2R3W0keFz7+xLedCAMniwmx65dBRLMteKQ9fI9Ii83A=";
-                });
-                doInstallCheck = false;
-              });
-            })
-          ];
-        };
+        pkgs = import nixpkgs { inherit system; };
         config = import ./config;
         nixvim' = nixvim.legacyPackages.${system};
         nvim = nixvim'.makeNixvimWithModule {
