2b33402f3e1b4f2c470f05c94409a0f55b036bfe
yadokani389 <yadokani389@gmail.com>
2025-08-08 21:04:54 +0900

    sandwich: add

--- config/default.nix
+++ config/default.nix
@@ -52,6 +52,7 @@
     markdown-preview.enable = true;
     lazygit.enable = true;
     render-markdown.enable = true;
+    sandwich.enable = true;
   };
 
   extraPlugins = with pkgs.vimPlugins; [ mkdir-nvim ];
